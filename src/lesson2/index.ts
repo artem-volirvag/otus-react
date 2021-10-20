4785c0cd7293d13e06fbcf90633ed6fd28ea43f4
artem.gavrilov <artem.gavrilov@russianpost.ru>
2021-10-20 22:35:05 +0300

    Исправления по codereview

--- src/lesson2/index.ts
+++ src/lesson2/index.ts
@@ -7,7 +7,7 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-const question = (): Promise<null> =>
+const question = (): Promise<void> =>
   new Promise((resolve) => {
     rl.question("> ", (answer: string) => {
       const result = runner(answer);
@@ -20,7 +20,7 @@ const question = (): Promise<null> =>
     });
   });
 
-async function app(): Promise<null> {
+async function app(): Promise<void> {
   while (true) {
     await question();
   }
