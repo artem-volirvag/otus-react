d7967210b9018525b4d5ff33c41e009a961608e4
artem.gavrilov <artem.gavrilov@russianpost.ru>
2021-12-23 20:22:26 +0300

    fix storybook

--- src/gol/components/FormLogin.stories.tsx
+++ src/gol/components/FormLogin.stories.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import FormLogin from './FormLogin';
+import { BrowserRouter } from 'react-router-dom';
 
 export default {
   title: 'Game_of_life/FormLogin',
   component: FormLogin,
 } as ComponentMeta<typeof FormLogin>;
 
-const Template: ComponentStory<typeof FormLogin> = (args) => (
-  <FormLogin {...args} />
+const Template: ComponentStory<typeof FormLogin> = () => (
+  <BrowserRouter>
+    <FormLogin />
+  </BrowserRouter>
 );
 
 export const FormLoginSample1 = Template.bind({});
-FormLoginSample1.args = {
-  onSubmit: () => null,
-};
